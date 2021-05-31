9eddec757b1f8c1f99f2445c4168c31190c061b8
Bas <bas@bassmit.info>
2021-05-31 13:07:06 +0200

    make less error prone as pointers to generic unmanaged types are allowed now

--- Assets/Mem.cs
+++ Assets/Mem.cs
@@ -3,9 +3,9 @@ using Unity.Collections.LowLevel.Unsafe;
 
 static unsafe class Mem
 {
-    public static void* Malloc<T>(Allocator allocator) where T : struct
+    public static T* Malloc<T>(Allocator allocator) where T : unmanaged
         => Malloc<T>(1, allocator);
 
-    public static void* Malloc<T>(int amount, Allocator allocator) where T : struct
-        => UnsafeUtility.Malloc(amount * UnsafeUtility.SizeOf<T>(), UnsafeUtility.AlignOf<T>(), allocator);
+    public static T* Malloc<T>(int amount, Allocator allocator) where T : unmanaged
+        => (T*) UnsafeUtility.Malloc(amount * UnsafeUtility.SizeOf<T>(), UnsafeUtility.AlignOf<T>(), allocator);
 }
