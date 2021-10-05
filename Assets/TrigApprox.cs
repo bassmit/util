427a63299101f71a5856ab0abd08afcc8989f31b
Bas <bas@bassmit.info>
2021-10-05 14:27:31 +0200

    wip

--- Assets/TrigApprox.cs
+++ Assets/TrigApprox.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Unity.Mathematics;
 using UnityEditor;
 using Debug = UnityEngine.Debug;
 using Random = Unity.Mathematics.Random;
